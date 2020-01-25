bf257be3d20c28c77c8c08b7f110928f2c108412
Stanley Fuller <stanthesoupking@gmail.com>
2020-01-26 09:12:29 +1100

    Progress

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # PiDotLCD
 Python driver for using the 128x64 dot LCD display on a Raspberry Pi
+
+## Basic Usage
+```python3
+import PiDotLCD
+
+# Create display
+display = PiDotLCD.display()
+
+display.set_pixel(16, 16) # Set pixel
+
+display.set_pixel(16, 16, False) # Clear pixel
+
+
+
+```
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stanthesoupking",
+    version="0.0.1",
+    author="Stanley Fuller",
+    author_email="stanthesoupking@gmail.com",
+    description="Python driver for using the 128x64 dot LCD display on a Raspberry Pi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/stanthesoupking/PiDotLCD",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
